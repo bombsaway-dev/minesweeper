3e0ee0229919187ed324d88c54da331068c2978a
sab26seto <setosabrina26@gmail.com>
2025-06-18 16:25:03 -0400

    the beginning!

---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style.css';
+
+
+
+
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Nav, Navbar, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
+
+function home() {
+  return (
+   <div>Bombs Away</div>
+    
+  );
+}
+
+export default home;
---
+++ src/pages/tutorial.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style.css';
+
+
+import { Link } from "react-router-dom";
+import { Carousel, Container, Row, Col, Nav, Navbar, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
+
+function tutorial() {
+  return (
+    <Carousel>
+        <Carousel.Item>
+            <img
+        </Carousel.Item>
+    </Carousel>
+    
+  );
+}
+
+export default tutorial;
